f4e66fe00164cb23e0810b30d0ff8584cdfbb194
sly545 <sylvainferrandes@gmail.com>
2023-11-28 01:12:48 +0100

    premier moutur du portfolio

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-    document.querySelector(".circle").addEventListener("click", function() {
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', function() {
+        this.querySelector('.video-preview').play();
+    });
+    card.addEventListener('mouseleave', function() {
+        this.querySelector('.video-preview').pause();
+    });
+});
+
+document.querySelector(".rectangle").addEventListener("click", function() {
+    document.querySelector("#introVideo").hidden = false;
+    document.querySelector("#introVideo").play();
+    document.querySelector("footer").classList.add('hidden'); // Masque le footer
+});
+
+document.querySelector("#introVideo").addEventListener("ended", function() {
+    document.querySelector("#intro").style.opacity = 0;
+    setTimeout(function() {
+        document.querySelector("#intro").style.display = "none";
+        document.querySelector("#main").style.display = "flex";
+        document.querySelector("#main").style.opacity = 1;
+        document.querySelector("footer").classList.remove('hidden'); // Réaffiche le footer
+    }, 2000); // Correspond à la durée de la transition CSS
+});
+
+document.querySelector("#replayLink").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector("#main").style.opacity = 0;
+    setTimeout(function() {
+        document.querySelector("#main").style.display = "none";
+        document.querySelector("#intro").style.display = "flex";
+        document.querySelector("#intro").style.opacity = 1;
+        document.querySelector("#introVideo").currentTime = 0;
         document.querySelector("#introVideo").hidden = false;
         document.querySelector("#introVideo").play();
-    });
-
-    document.querySelector("#introVideo").addEventListener("ended", function() {
-        document.querySelector("#intro").style.opacity = 0;
-        setTimeout(function() {
-            document.querySelector("#intro").style.display = "none";
-            document.querySelector("#main").style.display = "flex";
-            document.querySelector("#main").style.opacity = 1;
-        }, 2000); // Correspond à la durée de la transition CSS
-    });
-
-    document.querySelector("#replayLink").addEventListener("click", function(event) {
-        event.preventDefault();
-        document.querySelector("#main").style.opacity = 0;
-        setTimeout(function() {
-            document.querySelector("#main").style.display = "none";
-            document.querySelector("#intro").style.display = "flex";
-            document.querySelector("#intro").style.opacity = 1;
-            document.querySelector("#introVideo").currentTime = 0;
-            document.querySelector("#introVideo").hidden = false;
-            document.querySelector("#introVideo").play();
-        }, 2000); // Correspond à la durée de la transition CSS
-    });
+        document.querySelector("footer").classList.add('hidden'); // Masque à nouveau le footer lors du replay
+    }, 2000); // Correspond à la durée de la transition CSS
+});
